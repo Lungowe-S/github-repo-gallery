190dcc4cf496905c865f97968d8e0b046168b826
lungowe_s <lungowe.simasiku1@gmail.com>
2022-10-16 16:32:53 +0200

    Added async function to fetch my GitHub repos and inclded a function 'displayRepos' to display the repos in an unordered list

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 
 // Select the div with the class of overview
 const overview = document.querySelector(".overview")
-
 // GitHub username
 const username = "Lungowe"
+// Select the unordered list that will display the repos list
+const repoList = document.querySelector(".repo-list")
 
 /* ==== Fetch API/JSON Data ==== */
 async function fetchProfile() {
     const request = await fetch(`https://api.github.com/users/${username}`)
     const response = await request.json()
-    console.log(response)
+    //console.log(response)
     displayInfo(response)
 }
 
@@ -30,4 +31,25 @@ const displayInfo = (response) => {
         <p><strong>Number of public repos:</strong> ${response.public_repos}</p>
     </div>`
     overview.append(div)
+    fetchRepos()
+}
+
+/* ==== Async function to fetch your repos ==== */
+async function fetchRepos() {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated?per_page=100?type=public`)
+    const repos = await fetchRepos.json()
+    //console.log(repos)
+    displayRepos(repos)
+}
+
+//fetchRepos()
+
+/* ===== Function to display info about your repos ==== */
+const displayRepos = (repos) => {
+    for (const repo of repos) {
+        const listItem = document.createElement("li")
+        listItem.classList.add("repo")
+        listItem.innerHTML = `<h3>${repo.name}</h3>`
+        repoList.append(listItem)
+    }
 }
